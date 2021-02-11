b2b37d7ac1b5999d6bef6cf795d5e145c15d3706
Anwesh43 <anweshthecool0@gmail.com>
2021-02-11 19:03:29 +0530

    adding LRUDNode class

--- index.ts
+++ index.ts
@@ -135,3 +135,45 @@ class Animator {
         }
     }
 }
+
+class LRUDNode {
+
+    next : LRUDNode 
+    prev : LRUDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRUDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLRUDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir :  number, cb : Function) : LRUDNode {
+        var curr : LRUDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
