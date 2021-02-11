40a8c55bda295dcd0149a5a252198c8cd95638f6
Anwesh43 <anweshthecool0@gmail.com>
2021-02-11 19:06:50 +0530

    adding LineLeftUpDown class

--- index.ts
+++ index.ts
@@ -177,3 +177,26 @@ class LRUDNode {
         return this 
     }
 }
+
+class LineLeftUpDown {
+
+    curr : LRUDNode = new LRUDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
